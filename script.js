00c01db65b61cd5428a577d9e83d5717dc0e2df5
NicoleTipping <nacayard@gmail.com>
2025-09-04 17:17:57 -0500

    Add addBookToLibrary function and displayArray function to update cards with added book. Update style so form disappears after submit

--- script.js
+++ script.js
@@ -31,10 +31,11 @@ const myLibrary = [
     },
 ];
 
-myLibrary.forEach(book => {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card')
-    bookCard.innerHTML = `
+function displayArray() {
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card')
+        bookCard.innerHTML = `
         <div class="title">${book.title}</div>
         <div>By</div>
         <div class="author">${book.author}</div>
@@ -44,39 +45,50 @@ myLibrary.forEach(book => {
             <button type="delete" class="deleteButton">Delete</button>
         </div>
     `;
-    libraryConatiner.appendChild(bookCard);
+        libraryConatiner.appendChild(bookCard);
+    });
+};
+displayArray();
+
+newBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    document.querySelector('.newBookButton').style.display = "block"
+
+    document.querySelector('.form-container').style.display = "none"
+
+    title = e.target.elements[0].value
+    author = e.target.elements[1].value
+    pages = Number(e.target.elements[2].value)
+    if (document.querySelector('input[type="radio"]:checked')) {
+        read = true
+    } else {
+        read = false
+    }
+
+    addBookToLibrary(title, author, pages, read);
 });
 
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
+
+    myLibrary.push(newBook)
+
+    document.querySelector('.library-container').innerHTML = ""
+
+    displayArray()
+};
+
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.id = crypto.randomUUID()
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 };
 
 newBookButton.addEventListener("click", () => {
     document.querySelector('.form-container').style.display = "block";
     document.querySelector('.newBookButton').style.display = "none";
 });
-
-newBookForm.addEventListener("submit", (e) => {
-    document.querySelector('.newBookButton').style.display = "block"
-
-    title = e.target.elements[0].value
-    author = e.target.elements[1].value
-    pages = Number(e.target.elements[2].value)
-    if(document.querySelector('input[type="radio"]:checked')){
-        read = true
-    } else {
-        read = false
-    }
-    
-    addBookToLibrary(title, author, pages, read)
-});
-
-function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
-    
-    return myLibrary.push(newBook)
-};
