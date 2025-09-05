489f81d903569f6dda28b1ecc5df6d0e48f65542
NicoleTipping <nacayard@gmail.com>
2025-09-05 14:08:50 -0500

    Add data attribute to delete button and implement delete functionality of book cards

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let libraryConatiner = document.querySelector('.library-container');
 let newBookButton = document.querySelector('.newBookButton');
 let newBookForm = document.querySelector('.newBookForm');
+let deleteButton = document.querySelector('.deleteButton')
 
-let title = '';
-let author = '';
-let pages = 0;
-let read = false;
+let title = ''
+let author = ''
+let pages = 0
+let read = false
 
 const myLibrary = [
     {
@@ -36,13 +37,14 @@ function displayArray() {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card')
         bookCard.innerHTML = `
+        <div class='id' hidden>${book.id}</div>
         <div class="title">${book.title}</div>
         <div>By</div>
         <div class="author">${book.author}</div>
         <div class="pages">${book.pages} pages</div>
         <div class="buttons">
             <button type="button" class="readButton">Read</button>
-            <button type="delete" class="deleteButton">Delete</button>
+            <button type="button" class="deleteButton" data-id="${book.id}" >Delete</button>
         </div>
     `;
         libraryConatiner.appendChild(bookCard);
@@ -71,7 +73,6 @@ newBookForm.addEventListener("submit", (e) => {
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
 
     myLibrary.push(newBook)
 
@@ -91,4 +92,17 @@ function Book(title, author, pages, read) {
 newBookButton.addEventListener("click", () => {
     document.querySelector('.form-container').style.display = "block";
     document.querySelector('.newBookButton').style.display = "none";
+});
+
+libraryConatiner.addEventListener('click', (e) => {
+    if (e.target.classList.contains('deleteButton')) {
+        let id = e.target.dataset.id
+        let findBook = myLibrary.findIndex(book => book.id == id)
+
+        if (findBook !== -1) {
+            myLibrary.splice(findBook, 1)
+
+            e.target.closest('.book-card').remove()
+        }
+    }
 });
